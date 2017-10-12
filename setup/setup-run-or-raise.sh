39724c51ed5c7650d662046b373826b4700e7963
eugenesqr <eskurihin@gmail.com>
2017-10-12 10:52:59 +0300

    run-or-raise setup updated

--- setup/setup-run-or-raise.sh
+++ setup/setup-run-or-raise.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 progress "setting up run-or-raise"
-cp ./run-or-raise.sh ../run-or-raise
-chmod +x ../run-or-raise
+chmod +x ./run-or-raise
+ln -s ~/bin/dotfiles/run-or-raise ~/bin/run-or-raise
 pass "setting up run-or-raise"

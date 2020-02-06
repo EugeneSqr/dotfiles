3433c3b712be1252e89db88431b0d9077053bc3b
eugenesqr <eskurihin@gmail.com>
2020-02-06 13:09:07 +0300

    request both name and email when setting up git

--- setup/setup-git.sh
+++ setup/setup-git.sh
@@ -4,15 +4,16 @@ read should_setup_git
 if [ $should_setup_git = y ]; then
     gitconfig=~/.gitconfig
     if [ ! -f $gitconfig ]; then
+        echo -n "Enter github name: "
+        read github_name
         echo -n "Enter github email: "
         read github_email
         touch $gitconfig
         echo "[User]" >> $gitconfig
+        echo "    name = $github_name" >> $gitconfig
         echo "    email = $github_email" >> $gitconfig
         echo "[include]" >> $gitconfig
         echo "    path = bin/dotfiles/gitconfig" >> $gitconfig
-        echo "[core]" >> $gitconfig
-        echo "    autocrlf = input" >> $gitconfig
         pass "setting up git"
     else
         pass "setting up git (already set up)"

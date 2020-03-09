698c181dcbd54f7ad55b9c19429a70f92bcc6210
eugenesqr <eskurihin@gmail.com>
2020-03-09 10:20:19 +0300

    run-or-raise setup fixed

--- setup/setup-run-or-raise.sh
+++ setup/setup-run-or-raise.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 progress "setting up run-or-raise"
-chmod +x ./run-or-raise
+chmod +x ~/bin/dotfiles/run-or-raise
 if [ ! -f ~/bin/run-or-raise ]; then
     ln -s ~/bin/dotfiles/run-or-raise ~/bin/run-or-raise
 fi

56b6418d9cf0ddfad305cd3fd98f7435db029956
eugenesqr <eskurihin@gmail.com>
2020-07-13 23:14:22 +0300

    setting up mail accounts improved

--- setup/setup-mutt.sh
+++ setup/setup-mutt.sh
@@ -2,76 +2,47 @@
 function setup_account() {
     mkdir -p ~/.mutt/$2/cache/{headers,bodies}
     touch ~/.mutt/$2/certificates
-
     local account_muttrc=~/.mutt/$2/muttrc
-    if [ ! -f $account_muttrc ]; then
-        touch $account_muttrc
-    fi
-
-    appendLine $account_muttrc "set imap_user = \"$1\""
-    echo "Enter mail app password: "
-    local password
-    read password
-
-    appendLine $account_muttrc "set folder = \"imaps://imap.gmail.com:993\""
-    appendLine $account_muttrc "set spoolfile = \"+INBOX\""
-    appendLine $account_muttrc "set postponed = \"+[Gmail]/Drafts\""
-    appendLine $account_muttrc "set trash = \"+[Gmail]/Trash\""
-    appendLine $account_muttrc "set imap_pass = \"$password\""
-    appendLine $account_muttrc "set smtp_url = \"smtp://$2@smtp.gmail.com:587/\""
-    appendLine $account_muttrc "set smtp_pass = \"$password\""
-    appendLine $account_muttrc "set from = \"$1\""
-    appendLine $account_muttrc "set realname = \"Eugene Skurikhin\""
-    appendLine $account_muttrc "set header_cache=~/.mutt/$2/cache/headers"
-    appendLine $account_muttrc "set message_cachedir=~/.mutt/$2/cache/bodies"
-    appendLine $account_muttrc "set certificate_file=~/.mutt/$2/certificates"
-    appendLine $account_muttrc "bind  index g  noop"
-    appendLine $account_muttrc "macro index gi \"<change-folder>=INBOX<enter>\" \"Go to inbox\""
-    appendLine $account_muttrc "macro index gs \"<change-folder>=[Gmail]/Sent%20Mail<enter>\" \"Go to sent mail\""
-    appendLine $account_muttrc "macro index gt \"<change-folder>=[Gmail]/Trash<enter>\" \"Go to trash\""
-    appendLine $account_muttrc "bind  index,pager d  noop"
-    appendLine $account_muttrc "macro index,pager dd \":set delete=yes resolve=no<enter><clear-flag>N<save-message>=[Gmail]/Trash<enter><enter><sync-mailbox>\" \"Trash\""
-    appendLine $account_muttrc "bind  index,pager y  noop"
-    appendLine $account_muttrc "macro index,pager yy \":set delete=yes resolve=no<enter><clear-flag>N<save-message>=[Gmail]/All%20Mail<enter><enter><sync-mailbox>\" \"Archive\""
+    cp $dotfiles_dir/setup/$1.muttrc $account_muttrc
+    read -p "Username: " account_username
+    sed -i "s/<username>/$account_username/g" $account_muttrc
+    read -p "Host: " account_host
+    sed -i "s/<host>/$account_host/g" $account_muttrc
+    read -p "Real name: " account_real_name
+    sed -i "s/<real_name>/$account_real_name/g" $account_muttrc
+    read -s -p "Password: " account_password
+    sed -i "s/<password>/$account_password/g" $account_muttrc
+    sed -i "s/<local_folder_name>/$2/g" $account_muttrc
+    echo ""
 }
 
 read -p "Setup mutt? (y/n)? " should_setup_mutt
 if [ $should_setup_mutt = y ]; then
-    if [ ! -d ~/.mutt ]; then
-        mkdir ~/.mutt
+    mkdir -p ~/.mutt
 
-        account_names=()
-        while true; do
-            # setting up personal data
-            echo "Enter email address: "
-            read emailAddress # e.g. google@gmail.com
-            if [ "$emailAddress" == "" ]; then
-                break
-            fi
-            account_name=${emailAddress%@*}    # e.g. google
-            setup_account $emailAddress $account_name
-            account_names+=($account_name)
-        done
-
-        muttrc=~/.mutt/muttrc
-        if [ -f $muttrc ]; then
-            rm $muttrc
+    local_folder_names=()
+    while true; do
+        read -p "Email type (gmail/outlook): " email_type
+        if [ "$email_type" == "" ]; then
+            break
         fi
-        touch $muttrc
-        appendLine $muttrc "source $dotfiles_dir/mutt/muttrc"
-        appendLine $muttrc "source $dotfiles_dir/mutt/colorscheme.muttrc"
-        # default config
-        appendLine $muttrc "source ~/.mutt/${account_names[0]}/muttrc"
+        read -p "Local folder name (alias): " local_folder_name
+        setup_account $email_type $local_folder_name
+        local_folder_names+=($local_folder_name)
+    done
 
-        # Macros for switching accounts
-        for i in "${!account_names[@]}"; do
-            appendLine $muttrc "macro index <f$((i+2))> '<sync-mailbox><enter-command>source ~/.mutt/${account_names[$i]}/muttrc<enter><change-folder>!<enter>'"
-        done
-
-        mailcap=~/.mutt/mailcap
-        touch $mailcap
-        appendLine $mailcap "text/html; lynx -assume_charset=%{charset} -display_charset=utf-8 -dump %s; nametemplate=%s.html; copiousoutput"
-        appendLine $mailcap "application/rtf; okular %s;"
-        appendLine $mailcap "application/pdf; okular %s;"
-    fi
+    muttrc=~/.mutt/muttrc
+    touch $muttrc
+    appendLine $muttrc "source $dotfiles_dir/mutt/muttrc"
+    appendLine $muttrc "source $dotfiles_dir/mutt/colorscheme.muttrc"
+    # default config
+    appendLine $muttrc "source ~/.mutt/${local_folder_names[0]}/muttrc"
+    # switching accounts
+    for i in "${!local_folder_names[@]}"; do
+        appendLine $muttrc "macro index <f$((i+2))> '<sync-mailbox><enter-command>source ~/.mutt/${local_folder_names[$i]}/muttrc<enter><change-folder>!<enter>'"
+    done
+    mailcap=~/.mutt/mailcap
+    cp $dotfiles_dir/setup/mailcap ~/.mutt/
+    read -p "Open PDF with: " pdf_viewer
+    sed -i "s/<pdf_viewer>/$pdf_viewer/g" $mailcap
 fi

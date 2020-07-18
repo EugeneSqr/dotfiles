59da4e19b5a8c88bfd5953ff822fb6397825baa8
eugenesqr <eskurihin@gmail.com>
2020-07-18 11:13:02 +0300

    git config moved to .config

--- setup/setup-git.sh
+++ setup/setup-git.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 read -p "Setup git (y/n)? " should_setup_git
 if [ $should_setup_git = y ]; then
-    gitconfig=~/.gitconfig
-    cp ./setup/template.gitconfig $gitconfig
+    gitconfig_dir=$XDG_CONFIG_HOME/git
+    gitconfig=$gitconfig_dir/config
+    mkdir -p "$gitconfig_dir" && cat ./setup/template.gitconfig > "$gitconfig"
     read -p "Enter github name: " input_name
-    sed -i "s/<name>/$input_name/g" $gitconfig
+    sed -i "s/<name>/$input_name/g" "$gitconfig"
     read -p "Enter github email: " input_email
-    sed -i "s/<email>/$input_email/g" $gitconfig
-    sed -i "s:<path>:$dotfiles_dir\/gitconfig:g" $gitconfig
+    sed -i "s/<email>/$input_email/g" "$gitconfig"
+    sed -i "s:<path>:$dotfiles_dir\/gitconfig:g" "$gitconfig"
 fi

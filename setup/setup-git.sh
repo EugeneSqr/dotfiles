95ca7e5d8674897ff7e2d4e6cafcf1a74287744c
eugenesqr <eskurihin@gmail.com>
2020-10-12 15:08:24 +0300

    do not set default username and email for git repos

--- setup/setup-git.sh
+++ setup/setup-git.sh
@@ -4,9 +4,5 @@ if [ $should_setup_git = y ]; then
     gitconfig_dir=$XDG_CONFIG_HOME/git
     gitconfig=$gitconfig_dir/config
     mkdir -p "$gitconfig_dir" && cat ./setup/template.gitconfig > "$gitconfig"
-    read -p "Enter github name: " input_name
-    sed -i "s/<name>/$input_name/g" "$gitconfig"
-    read -p "Enter github email: " input_email
-    sed -i "s/<email>/$input_email/g" "$gitconfig"
     sed -i "s:<path>:$dotfiles_dir\/gitconfig:g" "$gitconfig"
 fi

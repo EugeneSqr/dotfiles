a40b2bd1af9e6e8a788753a43669fd0298c52118
eugenesqr <eskurihin@gmail.com>
2021-02-24 12:14:21 +0300

    git setup simplified

--- setup/setup-git.sh
+++ setup/setup-git.sh
@@ -2,7 +2,5 @@
 read -p "Setup git (y/n)? " should_setup_git
 if [ $should_setup_git = y ]; then
     gitconfig_dir=$XDG_CONFIG_HOME/git
-    gitconfig=$gitconfig_dir/config
-    mkdir -p "$gitconfig_dir" && cat ./setup/template.gitconfig > "$gitconfig"
-    sed -i "s:<path>:$dotfiles_dir\/gitconfig:g" "$gitconfig"
+    mkdir -p "$gitconfig_dir" && ln -s "$dotfiles_dir/git/.gitconfig" "$gitconfig_dir/config"
 fi

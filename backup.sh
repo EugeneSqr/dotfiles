af6bba41b06d2d624f4c9ee25f838a31e92ab92d
eugenesqr <eskurihin@gmail.com>
2021-08-24 10:47:52 +0300

    encrypt backups with gpg

--- backup.sh
+++ backup.sh
@@ -16,18 +16,16 @@ archive_file="$hostname-$day.tgz"
 
 # Print start status message.
 echo "Backing up $yandex_disk $documents to $dest/$archive_file"
-date
-echo
 
 # Backup the files using tar.
-tar --exclude $yandex_disk/.sync \
-    --exclude $yandex_disk/.private \
-    -czf $dest/$archive_file $yandex_disk $documents
+tar --exclude "$yandex_disk/.sync" \
+    --exclude "$yandex_disk/.private" \
+    -czf "$dest/$archive_file" "$yandex_disk" "$documents" && \
+    gpg -o "$dest/$archive_file.gpg" --symmetric "$dest/$archive_file" && \
+    rm "$dest/$archive_file"
 
 # Print end status message.
-echo
 echo "Backup finished"
-date
 
 # Long listing of files in $dest to check file sizes.
 ls -lh $dest

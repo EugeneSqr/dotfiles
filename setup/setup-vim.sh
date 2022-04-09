cf9a6feb9bf51925d93f77d395101307028bac7d
eugenesqr <eskurihin@gmail.com>
2022-04-09 12:04:08 +0300

    removed redundant fonts setup

--- setup/setup-vim.sh
+++ setup/setup-vim.sh
@@ -18,18 +18,3 @@ if [ ! -d ~/.vim/bundle ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
 fi
-# Powerline fonts for vim Airline
-font_dir=~/.local/share/fonts
-font_name=PowerlineSymbols.otf
-mkdir -p $font_dir
-if [ ! -f $font_dir/$font_name ]; then
-    (cd $font_dir && curl --remote-name --location https://github.com/Lokaltog/powerline/raw/develop/font/$font_name)
-    fc-cache -f -v
-fi
-
-font_config_dir=~/.config/fontconfig/fonts.conf
-font_config_name=10-powerline-symbols.conf
-mkdir -p $font_config_dir
-if [ ! -f $font_config_dir/$font_config_name ]; then
-    (cd $font_config_dir && curl --remote-name --location https://github.com/Lokaltog/powerline/raw/develop/font/$font_config_name)
-fi

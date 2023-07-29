e2a01ea2ee4285a7322ea2dc6d52314a4178bda8
eugenesqr <eskurihin@gmail.com>
2023-07-29 10:39:01 +0300

    ultisnips setup

--- setup/setup-vim.sh
+++ setup/setup-vim.sh
@@ -6,15 +6,15 @@ append_line ~/.vim/vimrc ":so $dotfiles_dir/vim/vimrc"
 curl -O http://ftp.vim.org/vim/runtime/spell/ru.utf-8.spl \
      -O http://ftp.vim.org/vim/runtime/spell/ru.utf-8.sug)
 # Russian personal dictionary
-if [ ! -f ~/.vim/spell/ru.utf-8.add ]; then
-    ln -s $dotfiles_dir/vim/spell/ru.utf-8.add ~/.vim/spell/ru.utf-8.add
-fi
+ln -sf $dotfiles_dir/vim/spell/ru.utf-8.add ~/.vim/spell/ru.utf-8.add
 # English personal dictionary
-if [ ! -f ~/.vim/spell/en.utf-8.add ]; then
-    ln -s $dotfiles_dir/vim/spell/en.utf-8.add ~/.vim/spell/en.utf-8.add
-fi
+ln -sf $dotfiles_dir/vim/spell/en.utf-8.add ~/.vim/spell/en.utf-8.add
+
 # Vundle + plugins
 if [ ! -d ~/.vim/bundle ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
 fi
+
+# ultisnips snippets
+ln -sf $dotfiles_dir/vim/UltiSnips ~/.vim/UltiSnips

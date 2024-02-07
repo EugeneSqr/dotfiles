47a7d3a270aea49b29ceb037755991a433ec14f9
eugenesqr <eskurihin@gmail.com>
2024-02-07 19:19:36 +0300

    fix xfce4-terminal setup from scratch

--- setup/setup-xfce4-terminal.sh
+++ setup/setup-xfce4-terminal.sh
@@ -2,5 +2,6 @@
 set -e
 
 if [ ! -f "$XDG_CONFIG_HOME/xfce4/terminal/terminalrc" ]; then
+    mkdir -p "$XDG_CONFIG_HOME/xfce4/terminal/"
     ln -s "$dotfiles_dir/xfce4-terminal/terminalrc" "$XDG_CONFIG_HOME/xfce4/terminal/"
 fi

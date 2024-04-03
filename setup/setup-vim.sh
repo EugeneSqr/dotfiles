5a8df02402b2b867a98bc4b906a4a02613821263
eugenesqr <eskurihin@gmail.com>
2024-04-03 12:46:27 +0300

    do not download vim spell files if they exist locally

--- setup/setup-vim.sh
+++ setup/setup-vim.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p ~/.vim/spell
+spell_dir=~/.vim/spell
+mkdir -p "$spell_dir"
 append_line ~/.vim/vimrc ":so $dotfiles_dir/vim/vimrc"
 # Russian spellcheck
-(cd ~/.vim/spell && \
-curl -O http://ftp.vim.org/vim/runtime/spell/ru.utf-8.spl \
-     -O http://ftp.vim.org/vim/runtime/spell/ru.utf-8.sug)
-# Russian personal dictionary
-ln -sf $dotfiles_dir/vim/spell/ru.utf-8.add ~/.vim/spell/ru.utf-8.add
-# English personal dictionary
-ln -sf $dotfiles_dir/vim/spell/en.utf-8.add ~/.vim/spell/en.utf-8.add
+# downloading the files can be slow, skip it if the file is already there
+if [ ! -f "$spell_dir/ru.utf-8.spl" ]; then
+    (cd "$spell_dir" && \
+    curl -O 'http://ftp.vim.org/vim/runtime/spell/ru.utf-8.spl' \
+         -O 'http://ftp.vim.org/vim/runtime/spell/ru.utf-8.sug')
+    # Russian personal dictionary
+    ln -sf "$dotfiles_dir/vim/spell/ru.utf-8.add" "$spell_dir/ru.utf-8.add"
+    # English personal dictionary
+    ln -sf "$dotfiles_dir/vim/spell/en.utf-8.add" "$spell_dir/en.utf-8.add"
+fi
 
 # Vundle + plugins
 if [ ! -d ~/.vim/bundle ]; then

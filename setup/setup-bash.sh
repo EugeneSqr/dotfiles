b08bbf2f35e748e0ad8293a29b627bde2ab11063
eugenesqr <eskurihin@gmail.com>
2026-07-28 12:25:30 +0300

    rename bashrc to .bashrc for better filetype detection
    git delta fails to properly highlight changes in the bashrc file
    treating it as a regular text file.

--- setup/setup-bash.sh
+++ setup/setup-bash.sh
@@ -2,5 +2,5 @@
 set -e
 
 append_line ~/.bashrc "dotfiles_dir=$dotfiles_dir"
-append_line ~/.bashrc '. $dotfiles_dir/bash/bashrc'
+append_line ~/.bashrc '. $dotfiles_dir/bash/.bashrc'
 append_line ~/.bashrc 'alias dot="cd $dotfiles_dir"'
